fd7cd1805110acedc0a39804aab5f084734f7a12
admin <eeoneguifeed@gmail.com>
2023-04-17 21:02:16 +0600

    Node class added.

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,4 +1,15 @@
 public class MyLinkedList<T> implements MyList<T> {
+    private class Node {
+        T element;
+        Node next;
+        Node prev;
+
+        public Node(T element, Node next, Node prev) {
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
     @Override
     public int size() {
         return 0;
