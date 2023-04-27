ae1ab6cea6e84e043ed30198dca8b80869b70f41
admin <eeoneguifeed@gmail.com>
2023-04-27 20:30:44 +0600

    Push() method created.

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,7 +1,10 @@
 import java.util.EmptyStackException;
-public class MyArrayListStack {
+public class MyArrayListStack<T> {
     private MyArrayList arr;
     MyArrayListStack(){
         arr = new MyArrayList();
+    }
+    public void push(T item){
+        arr.add(item, 0);
     }
 }
