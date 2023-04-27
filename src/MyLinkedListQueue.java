89d9149ee2ed232ebd37f77df5949d7b2f62b998
admin <eeoneguifeed@gmail.com>
2023-04-27 21:01:32 +0600

    Constructor created.

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,4 +1,7 @@
 import java.util.NoSuchElementException;
 public class MyLinkedListQueue<T>{
     private MyLinkedList<T> arr;
+    public MyLinkedListQueue() {
+        arr = new MyLinkedList<T>();
+    }
 }
