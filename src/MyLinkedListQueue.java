9e9d0ce4c97090e3bf5bdd1e7242eecabbc1104b
admin <eeoneguifeed@gmail.com>
2023-04-27 21:06:09 +0600

    dequeue() method added.

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -7,4 +7,9 @@ public class MyLinkedListQueue<T>{
     public void enqueue(T item) {
         arr.add(item, arr.size() - 1);
     }
+    public T dequeue() {
+        T element = (T) arr.get(0);
+        arr.remove(0);
+        return element;
+    }
 }
