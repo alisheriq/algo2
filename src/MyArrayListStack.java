60e6c54ad5d4c2d0ce5baea2f7e61634dff86655
admin <eeoneguifeed@gmail.com>
2023-04-27 20:47:21 +0600

    isEmpty method added.

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,18 +1,27 @@
 import java.util.EmptyStackException;
 public class MyArrayListStack<T> {
     private MyArrayList arr;
-    MyArrayListStack(){
+
+    MyArrayListStack() {
         arr = new MyArrayList();
     }
-    public void push(T item){
+
+    public void push(T item) {
         arr.add(item, 0);
     }
-    public T pop(){
+
+    public T pop() {
         T element = (T) arr.get(0);
         arr.remove(0);
         return element;
     }
-    public T peek(){
+
+    public T peek() {
         return (T) arr.get(0);
     }
+    public boolean isEmpty() {
+        if (arr.size() == 0) return true;
+        return false;
+    }
 }
+
